51afdf9594f31bb3fbffc95204e53bb7e83c9591
Aa <>
2025-05-23 09:28:08 +0300

    init

---
+++ CryptoTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoTracker
+//
+//  Created by anisko on 23.05.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
